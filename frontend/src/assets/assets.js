e59a86f5e9c23a312ba9b6638dc53e3ae302d3bd
leena.sharma <leena.sharma@tothenew.com>
2025-06-20 19:16:28 +0530

    2) tailwind css install

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,76 @@
+import pic1 from "./pic1.jpeg";
+import pic2 from "./pic2.jpeg";
+import pic3 from "./pic3.jpeg";
+import pic4 from "./pic4.jpeg";
+import pic5 from "./pic5.jpeg";
+
+export const assets = {
+  pic1,
+  pic2,
+  pic3,
+  pic4,
+  pic5,
+};
+
+export const products = [
+  {
+    _id: "1",
+    name: "First Interior",
+    description: " This is first interior",
+    price: 100,
+    image: [pic1],
+    category: "House interior",
+    subCategory: "House",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "2",
+    name: "Second Interior",
+    description: " This is Second interior",
+    price: 100,
+    image: [pic2],
+    category: "House interior",
+    subCategory: "House",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "3",
+    name: "Third Interior",
+    description: " This is third interior",
+    price: 100,
+    image: [pic3],
+    category: "House interior",
+    subCategory: "House",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "4",
+    name: "Fourth Interior",
+    description: " This is fourth interior",
+    price: 100,
+    image: [pic4],
+    category: "House interior",
+    subCategory: "House",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "5",
+    name: "Fifth Interior",
+    description: " This is fifth interior",
+    price: 100,
+    image: [pic5],
+    category: "House interior",
+    subCategory: "House",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+];
