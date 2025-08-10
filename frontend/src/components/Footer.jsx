f79d250e86a3715a9bbd3e5754a306d8f451ad98
leena.sharma <leena.sharma@tothenew.com>
2025-08-10 15:01:02 +0530

    components created

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,36 +3,38 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
-      <div>
-        <img src={assets.logo} className="mb-5 w-10 h-10" alt="" />
-        <p className="w-full md:w-2/3 text-gray-600">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
-      </div>
-      <div>
-        <p className="text-xl font-medium mb-5">COMPANY</p>
-        <ul className="flex flex-col gap-1 text-gray-600">
-          <li>Home</li>
-          <li>About us</li>
-          <li>Delivery</li>
-          <li>Privacy policy</li>
-        </ul>
-      </div>
-      <div>
-        <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
-        <ul className="flex flex-col gap-1 text-gray-600">
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-10 h-10" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
             <li>+1-212-456-7890</li>
             <li>ecom@gmail.com</li>
-        </ul>
+          </ul>
+        </div>
       </div>
       <div>
-        <hr/>
+        <hr />
         <p className="py-5 text-sm text-center">
-            Copyright 2025 ecom.com- All right reserved.
+          Copyright 2025 ecom.com- All right reserved.
         </p>
       </div>
     </div>
